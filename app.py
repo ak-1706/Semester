08f4ca4c6746bbe7bf813813899e0efd7ecc3884
Ak <akshaymali38@hotmail.com>
2020-10-07 15:36:01 +0530

    Update app.py
    Working

--- app.py
+++ app.py
@@ -1,53 +1,145 @@
-from flask import Flask,jsonify
-#from attendance import getAttendance
+from flask import Flask, request, make_response, jsonify,Response
+
+import json
 import pandas as pd
-app=Flask(__name__)
 
-@app.route("/")
-def welcome():
-    info={
-        "test":"test sucess",
-        "attendance":"/attendance/roll_no/div"
-        }
-    return jsonify(info)
+# initialize the flask app
+app = Flask(__name__)
 
-@app.route("/attendance/<roll_no>/<div>")
-def attendance(roll_no,div):
-    roll_no=int(roll_no)
-    if div=="A":
+# default route
+@app.route('/',methods=['POST','GET'])
+def index():
+    return 'This is the home page nothing to see here....'
+
+
+
+# create a route for webhook
+@app.route('/webhook', methods=['POST'])
+def webhook():
+   
+    req_data = request.get_json()
+    scenename = req_data['scene']['name']
+    ssid = req_data['session']['id']
+    nextscene = req_data['scene']['next']['name']
+    rollno = req_data['session']['params']['chosenRollno']
+    divans = req_data['session']['params']['chosenDivision']
+    rollansdivans = 'Rollno:' + rollno + '\nDiv:' + divans
+    msg = 'Your attendance is'
+    if divans=="A":
        df=pd.read_csv("data/A.csv")
         
-    if div=="B":
+    if divans=="B":
        df=pd.read_csv("data/B.csv")
         
-    data=df.loc[df["roll_no"]==roll_no]
-    
-    if data["310244 ISEE"].values[0] > 50:
-        message="power attendace"
-    elif data["310244 ISEE"].values[0] < 50 and data["310244 ISEE"].values[0] > 75:
-        message="you must have 75 % to grant your semester"
-    elif data["310244 ISEE"].values[0] < 75 and data["310244 ISEE"].values[0] > 80:
-        message="good attendance"
-    elif data["310244 ISEE"].values[0] < 80:
-        message="exelent attendace"
-        
-    result={
-    "message":message,
-    "Name":str(data["Name "].values[0]),
-    "RollNo":str(int(data["Roll"].values[0])),
-    "TOC":str(data["310241 TOC"].values[0]),
-    "DBMS":str(data["310242 DBMS"].values[0]),
-    "SEPM":str(data["310243 SEPM"].values[0]),
-    "ISEE":str(data["310244 ISEE"].values[0]),
-    "Attendance":str(data["Theory Attendance %"].values[0])
-    }
-    print(result)
-    
-    info={
-        "test":"test sucess"
-        }
-    
-    return  jsonify(result)
-    
+    data=df.loc[df["roll_no"]==int(rollno)]
 
-app.run(debug=True)
+
+
+
+    
+      
+
+    a = {'session':{
+            'id':ssid,
+            'params':{}
+        },
+        'prompt':{
+        'override':False,
+        "content": {
+        "table": {
+          "button": {},
+          "columns": [
+            {
+              "header": "Subject"
+            },
+            {
+              "header": "Present"
+            }
+            
+          ],
+          "image": {
+            "alt": "Google Assistant logo",
+            "height": 0,
+            "url": "https://developers.google.com/assistant/assistant_96.png",
+            "width": 0
+          },
+          "rows": [
+            {
+              "cells": [
+                {
+                  "text": "TOC"
+                },
+                {
+                  "text": str(data["310241 TOC"].values[0])
+                }
+              ]
+            },
+            {
+              "cells": [
+                {
+                  "text": "DBMS"
+                },
+                {
+                  "text": str(data["310242 DBMS"].values[0])
+                }
+              ]
+            },
+            {
+              "cells": [
+                {
+                  "text": "CN"
+                },
+                {
+                  "text": str(data["310245 CN"].values[0])
+                }
+              ]
+            },
+            {
+              "cells": [
+                {
+                  "text": "SEPM"
+                },
+                {
+                  "text":str(data["310243 SEPM"].values[0])
+                }
+              ]
+            },
+            {
+              "cells": [
+                {
+                  "text": "ISEE"
+                },
+                {
+                  "text": str(data["310244 ISEE"].values[0])
+                }
+              ]
+            }
+          ],
+          "subtitle": str(data["Theory Attendance %"].values[0]),
+          "title": str(data["Name "].values[0])
+        }
+      },
+
+        'firstSimple':{
+        'speech':msg,
+        'text':msg
+        }
+        },
+        'scene':{
+        'name':scenename,
+        'slots':{},
+        'next':{
+        'name':nextscene
+        }
+        }
+        }
+    ptoj = json.dumps(a)
+    return ptoj
+    
+# run the app
+if __name__ == '__main__':
+   app.run(debug=True)
+
+
+
+
